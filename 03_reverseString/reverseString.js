2f59d4f87995ff8e9306248500e574789235e5e4
vagueposting <42937049+vagueposting@users.noreply.github.com>
2025-01-15 03:01:33 +0800

    Answered Exercises 02 and 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let letters = word.split("")
+    let reverse = ""
+    for (let i = 0; i < word.length; i++) {
+        reverse += letters.pop();
+    }
+    return reverse;
 };
 
 // Do not edit below this line
