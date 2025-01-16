406fd8170884293a4cf7106030c42a352ef06ae6
vagueposting <42937049+vagueposting@users.noreply.github.com>
2025-01-16 15:33:31 +0800

    Sum All exercise accomplished

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
+const sumAll = function(min, max) {
+    if (!Number.isInteger(min) || !Number.isInteger(max)) return 'ERROR';
+    if (min < 0 || max < 0) return "ERROR";
+    if (min > max) {
+        [min, max] = [max, min]
+    }
 
+    let sum = 0;
+    for (i = min; i <= max; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
