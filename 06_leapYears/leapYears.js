2a1c705808476c9e2cc5b4be6c56d04babf9a28e
vagueposting <42937049+vagueposting@users.noreply.github.com>
2025-01-16 15:42:11 +0800

    Completed exercise 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,13 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    const yearDivisibleByFour = year % 4 === 0;
+    const isCentury = year % 100 === 0;
+    const fourHundredYears = year % 400 === 0;
 
+    if (yearDivisibleByFour && (!isCentury || fourHundredYears)) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
