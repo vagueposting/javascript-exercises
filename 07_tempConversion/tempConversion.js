8774d951da9dbabfe9eda6eb3eb582add10abb51
vagueposting <42937049+vagueposting@users.noreply.github.com>
2025-01-16 16:01:06 +0800

    Completed Exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  const degreesCelsius = (temp-32) * (5/9)
+  if (degreesCelsius == 0) { 
+    return 0;
+  } else {
+    return Number(degreesCelsius.toFixed(1));
+  }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  const degreesFahrenheit = (temp * (9/5)) + 32
+  if (degreesFahrenheit == 0) {
+    return 0;
+  } else {
+    return Number(degreesFahrenheit.toFixed(1));
+  }
 };
 
 // Do not edit below this line
